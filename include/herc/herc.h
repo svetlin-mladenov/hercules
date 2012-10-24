098eba675fbce4837b16056a64c6a405e0d1e637
Svetlin Mladenov <svetlin.mladenov@gmail.com>
2012-10-24 11:16:39 +0300

    Added assert_no_null

--- include/herc/herc.h
+++ include/herc/herc.h
@@ -1,6 +1,8 @@
 #ifndef HERC_HERC_H
 #define HERC_HERC_H
 
+#include <stdlib.h>
+
 #define _HERC_CASE_NAME_DELIMITER _D5a_
 
 //NOTE if you change the manglin schema you must also change the demangling schema in hercules-source-tree/src/mangler.c
@@ -8,8 +10,13 @@
 
 #define HTEST(test_case, test_name) void  _HERC_MANGLE(test_case, _HERC_CASE_NAME_DELIMITER, test_name) ()
 
+/*************
+ * assertions *
+ *************/
 
 #define assert_true(expr) if (!(expr)) {herc_fail("\"" #expr "\"" " expected to be true but was false");}
+
+#define assert_not_null(expr) if ((expr)==NULL) {herc_fail("\"" #expr "\"" " expected to be NULL but it isn't");}
 
 void herc_fail(const char *);
 
